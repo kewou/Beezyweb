860a32aae3b5365ec56bfae5ea4db77cfa351b1e
Kewou <kewou.noumia@gmail.com>
2018-04-04 16:35:43 +0200

    Intégration du template Landed

--- src/PublicBundle/Resources/public/js/displayDivUser.js
+++
@@ -1,9 +0,0 @@
-window.onload=function(){ 
-	$("#utilisateurPrivee").mouseenter(function(){
-		$("#blockCompte").show();		
-	});
-	$("#utilisateurPrivee").mouseleave(function(){
-		$("#blockCompte").hide();	
-	});	
-};
-
--- src/PublicBundle/Resources/public/js/jquery.diaporama/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-	$(".diaporama").diaporama({
-		animationSpeed: "normal",
-		delay:4
-	});
-
-});
