d0ef55e5af779ab27c5a56b1115e1d23b4e44a11
Joel NOUMIA <joel.noumia@annuaire.groupe.local>
2018-05-23 14:13:23 +0200

    Appel de l'inscription en Ajax

---
+++ src/UserBundle/Resources/public/js/Inscription.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(document).ready(function(){
+    $("#formInscription").submit(function(event) {
+        event.preventDefault();                        
+        var data =$("#formInscription").serialize();
+        $.post("/Beezyweb/web/app_dev.php/inscription",data,function(){
+            window.location.replace("/Beezyweb/web/app_dev.php/");                                       
+        });                           
+    });
+});
