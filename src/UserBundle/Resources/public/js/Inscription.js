a9ffc48316df7a5127a1e323bca24b4d1dd7aa8b
Joel NOUMIA <joel.noumia@annuaire.groupe.local>
2018-05-25 16:25:19 +0200

    enable toolbar symfony

--- src/UserBundle/Resources/public/js/Inscription.js
+++ src/UserBundle/Resources/public/js/Inscription.js
@@ -7,7 +7,11 @@
 
 $(document).ready(function(){
     $("#formInscription").submit(function(event) {
-        event.preventDefault();                        
+        event.preventDefault();
+        $("#formInscription").validate({
+            debug: true
+        });
+ 
         var data =$("#formInscription").serialize();
         $.post("/Beezyweb/web/app_dev.php/inscription",data,function(){
             window.location.replace("/Beezyweb/web/app_dev.php/");                                       
