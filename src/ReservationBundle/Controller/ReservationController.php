4241467a6a66d19c917ba9042ece40e11c702f5e
Kewou <kewou.noumia@gmail.com>
2018-08-20 21:57:57 +0200

    checbox 3

---
+++ src/ReservationBundle/Controller/ReservationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ReservationBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+/**
+ * Description of ReservationController
+ *
+ * @author joel
+ */
+class ReservationController extends Controller{
+    
+    
+    function reserverAction(){
+        
+    }
+}
