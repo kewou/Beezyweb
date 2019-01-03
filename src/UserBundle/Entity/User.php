2b49d8819b3fc7664f8012ec866554ccca9ce917
ANNUAIRE\frup73532 <kewou.noumia@gmail.com>
2019-01-03 11:26:52 +0100

    Mise à jour des dépendances

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -4,7 +4,7 @@ namespace UserBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
-use FOS\UserBundle\Entity\User as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints\Email;
